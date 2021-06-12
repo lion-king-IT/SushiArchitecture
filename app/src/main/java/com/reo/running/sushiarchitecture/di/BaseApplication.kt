7b19d1e30536fa27520da9fc881698cbd33aeb52
github lion-king-IT <reo.runningg@gmail.com>
2021-06-12 12:28:08 +0900

    下地を作る

---
+++ app/src/main/java/com/reo/running/sushiarchitecture/di/BaseApplication.kt
@@ -0,0 +1,9 @@
+package com.reo.running.sushiarchitecture.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication : Application() {
+    
+}
